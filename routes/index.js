13638534f85dd540ccaf3c735e018aefe594676f
Theerasoon <Theerasoon@gmail.com>
2018-07-12 16:11:03 +0700

    Test Env Variable

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let token = process.env.LINE_ACCESS_TOKEN
+  let secret = process.env.LINE_SECRET
+  res.render('index', { title: 'Express', token, secret });
 });
 
 module.exports = router;
