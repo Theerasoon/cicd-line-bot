8583140b1c0226376aa106410e3969e1f914237a
Theerasoon <theerasoon@gmail.com>
2018-09-07 00:47:34 +0700

    Add Dialog and its controller class

--- controllers/dialog/MasterDialog.js
+++ controllers/dialog/MasterDialog.js
@@ -1,27 +1,60 @@
+var LineResponse = require('../../utilities/LineResponse.js')
+
 class MasterDialog {
-  constructor (user, message, session) {
+  constructor(user, message, session) {
     this.user = user
     this.message = message
     this.session = session
     this.eventResponses = {
-      textMessage: {}
+      onText: {},
+      onTextDefault: {}
     }
   }
 
-  action () {
+  textProcessing(message) {
+    throw "You need to implement `textProcessing()` if useTextProcessing is enabled!!";
+  }
+
+  config() {
+    let config = {
+      useTextProcessing: true
+    }
+    this.config = config
+  }
+
+  onText(trigger, action) {
+    const callbackFunction = { action }
+    this.eventResponses.onText[trigger] = callbackFunction
+  }
+
+  onTextDefault(action) {
+    const callbackFunction = { action }
+    this.eventResponses.onTextDefault = callbackFunction
+  }
+
+  action() {
     switch (this.message.type) {
-      case 'text' :
-        if (this.eventResponses.textMessage[this.message.text] !== undefined ) {
-          const r = this.eventResponses.textMessage[this.message.text](this.user, this.message, this.session)
+      case 'text':
+        let trigger = this.message.text
+        if (this.config.useTextProcessing) {
+          trigger = this.textProcessing(this.message)
         }
-      break
+        const eventMessage = this.eventResponses.onText[trigger]
+        if (eventMessage === undefined) {
+          return this.eventResponses.onTextDefault['action'](this.user, this.message, this.session, this.lineResponseFactory())
+        } else {
+          return this.eventResponses.onText[trigger]['action'](this.user, this.message, this.session, this.lineResponseFactory())
+        }
+      }
+  }
+
+  lineResponseFactory() {
+    const config = {
+      channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+      channelSecret: process.env.LINE_SECRET,
     }
+    return new LineResponse(config) 
   }
-
-  onTextMessage(triggerText, callback) {
-    this.eventResponses.textMessage[triggerText] = callback
-  }
-
 }
 
 module.exports = MasterDialog
