79a5be43d8738f85c6f7326153a3b61b3477429a
Theerasoon <theerasoon@gmail.com>
2018-09-07 23:26:52 +0700

    Add comment in Master Dialog

--- controllers/dialog/MasterDialog.js
+++ controllers/dialog/MasterDialog.js
@@ -1,6 +1,11 @@
 var LineResponse = require('../../utilities/LineResponse.js')
 
 class MasterDialog {
+  /**
+   * Class constructor
+   * @param {Object} user - an json object that contains user information which are {groupId, userId, type}
+   * @param {Object} message - an object that contains message information which are { type, id, text }
+   */
   constructor(user, message, session) {
     this.user = user
     this.message = message
@@ -11,10 +16,10 @@ class MasterDialog {
     }
   }
 
-  textProcessing(message) {
-    throw "You need to implement `textProcessing()` if useTextProcessing is enabled!!";
-  }
-
+  /**
+   * A configuration method
+   * @param {Object} message - an object that contains message information which are { type, id, text }
+   */
   config() {
     let config = {
       useTextProcessing: true
@@ -22,16 +27,37 @@ class MasterDialog {
     this.config = config
   }
 
+  /**
+   * A method that shoud be overrided by child class when text processing is enabled
+   * @param {Object} message - an object that contains message information which are { type, id, text }
+   */
+  textProcessing(message) {
+    throw "You need to implement `textProcessing()` if useTextProcessing is enabled!!";
+  }
+
+  /**
+   * A register method uses for handle text message
+   * @param {String} trigger - trigger message or the result of textProcessing() to fire action function
+   * @param {Function} action - a callback function
+   */
   onText(trigger, action) {
     const callbackFunction = { action }
     this.eventResponses.onText[trigger] = callbackFunction
   }
 
+  /**
+   * A register method uses for handle text message in case of trigger not found in onText() 
+   * @param {Function} action - a callback function
+   */
   onTextDefault(action) {
     const callbackFunction = { action }
     this.eventResponses.onTextDefault = callbackFunction
   }
 
+  /**
+   * A action function that select the function to call
+   * @return {LineResponse} - a retrun object
+   */
   action() {
     switch (this.message.type) {
       case 'text':
@@ -48,6 +74,10 @@ class MasterDialog {
       }
   }
 
+  /**
+   * A facetory function to create LineResponse object
+   * @return {LineResponse} - a retrun object
+   */
   lineResponseFactory() {
     const config = {
       channelAccessToken: process.env.LINE_ACCESS_TOKEN,
