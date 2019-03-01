3810b29f5f0703f136582a9a94d2217d67a88fb1
Theerasoon <Theerasoon@amivoicethai.com>
2019-03-01 16:19:17 +0700

    Add some dialog control in Game Open Image

--- controllers/dialog/GameOpenImageMainDialog.js
+++ controllers/dialog/GameOpenImageMainDialog.js
@@ -13,6 +13,8 @@ class GameOpenImageMainDialog extends MasterDialog {
   }
 
   textProcessing(message) {
+    if (message.type !== 'text') return message.text
+    if (!message.text.includes('สร้างเกม')) return 'สร้างเกม'
     return message.text
   }
 
@@ -20,6 +22,31 @@ class GameOpenImageMainDialog extends MasterDialog {
 
     this.onTextDefault(function(user, message, session, lineResponse) {
       const nextDialog = null
+      lineResponse.addTextMessage('เย็นไว้')
+      if(session['custom']['state'] == 'answer') {
+        session['custom']['question']['answer'].push(message.text)
+        let text = JSON.stringify(session['custom']['question'])
+        lineResponse.addTextMessage(text)
+      }
+      return { lineResponse, nextDialog }
+    })
+
+    this.onText('สร้างเกม', function (user, message, session, lineResponse) {
+      const nextDialog = 'GameOpenImageMainDialog'
+      session['custom']['state'] = 'create_game'
+      lineResponse.addTextMessage('เลือกรูปที่ต้องการแล้วสงมาได้เลย')
+      return { lineResponse, nextDialog }
+    })
+
+    this.onImage(function (user, message, session, lineResponse, image) {
+      const nextDialog = 'GameOpenImageMainDialog'
+      session['custom']['state'] = 'answer'
+      session['custom']['question'] = {
+        'question_image' : `https://cidc-line-bot.herokuapp.com/img/i/${image}`,
+        'answer': []
+      }
+      session['custom']['question'] = {}
+      lineResponse.addTextMessage('คำตอบของรูปนี้คืออะไรหล่ะ')
       return { lineResponse, nextDialog }
     })
   }
