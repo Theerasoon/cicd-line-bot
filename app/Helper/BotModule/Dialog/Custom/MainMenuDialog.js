1d8290e7ad7081e1c3959e609d3d130e0c48c06d
theerasoon <theerasoon@gmail.com>
2019-06-19 23:51:32 +0700

    Add intent image game to main menu dialog

--- app/Helper/BotModule/Dialog/Custom/MainMenuDialog.js
+++ app/Helper/BotModule/Dialog/Custom/MainMenuDialog.js
@@ -1,59 +1,112 @@
 'use strict'
 
 const MasterDailog = use('App/Helper/BotModule/Dialog/Base/MasterDialog')
+const ImageGameMainMenuDialog = use('App/Helper/BotModule/Dialog/Custom/ImageGameMainMenuDialog')
 
 class MainMenuDialog extends MasterDailog {
-    constructor (user, message, session) {
-        super(user, message, session)
-        this.enable_nlu = true
-        this.use_nlu = true
-        this.next_dialog = null
+    constructor(user, message, session, LineClient) {
+        super(user, message, session, LineClient)
         this.create()
     }
 
     nlu () {
-        console.log('Process NLU')
-        console.log(this.message)
         if (this.message.type !== 'text') {
-            console.log('Match : Text')
             return 'default'
         }
         if (!this.message.text.includes('ตึลตึล')) {
-            console.log('Exclude : ตึลตึล')
             return 'default'
         }
         if (this.message.text.includes('เกมทายภาพ')) {
-            console.log('Include : ตึลตึล')
             return 'image_game'
         }
-        console.log('No Match')
         return 'default'
     }
 
     create () {
         this.onText('image_game', async () => {
-            const response = [
-                { type: 'text', text: 'ยินดีต้อนรับสู่เกมทายภาพ วันนี้อยากจะทำอะไรหล่ะ'}
+            this.response = [
+                {
+                    "type": "flex",
+                    "altText": "Flex Message",
+                    "contents": {
+                        "type": "bubble",
+                        "direction": "ltr",
+                        "header": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "spacer"
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "สวัสดี วันนี้อยากทำอะไร ?",
+                                    "size": "lg",
+                                    "align": "start",
+                                    "weight": "bold",
+                                    "color": "#FFFFFF"
+                                },
+                                {
+                                    "type": "spacer",
+                                    "size": "xs"
+                                }
+                            ]
+                        },
+                        "footer": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "spacing": "none",
+                            "contents": [
+                                {
+                                    "type": "button",
+                                    "action": {
+                                        "type": "message",
+                                        "label": "สร้างเกมใหม่",
+                                        "text": "ตึลตึลขอสร้างเกมใหม่"
+                                    },
+                                    "height": "sm"
+                                },
+                                {
+                                    "type": "button",
+                                    "action": {
+                                        "type": "message",
+                                        "label": "ดูเกมของฉัน",
+                                        "text": "ตึลตึลขอดูเกมของฉัน"
+                                    },
+                                    "height": "sm",
+                                    "gravity": "center"
+                                }
+                            ]
+                        },
+                        "styles": {
+                            "header": {
+                                "backgroundColor": "#00A6ED"
+                            },
+                            "footer": {
+                                "backgroundColor": "#F1F1F1"
+                            }
+                        }
+                    }
+                }
             ]
-            this.session['a'] = 'b'
-            this.next_dialog = 'MainMenuDialog'
-            this.response = response
+            this.next_dialog = 'ImageGameMainMenuDialog'
             return null
         })
 
-        this.onTextDefault(async () => {
-            const response = [
-                { type: 'text', text: 'ไม่มี response' }
-            ]
-            return "onDefault"
-        })
+        // this.onImage(async () => {
+        //   this.response.push({
+        //     type: 'text',
+        //     text: 'process image'
+        //   })
+        // })
 
-        this.onImage(async () => {
-          this.response.push({
-            type: 'text',
-            text: 'process image'
-          })
-        })
+        // this.onTextDefault(async () => {
+        //     const response = [
+        //         { type: 'text', text: 'ไม่มี response' }
+        //     ]
+        //     this.response = response
+        //     return null
+        // })
     }
 
 }
